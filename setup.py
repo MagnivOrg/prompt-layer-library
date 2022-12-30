e00aad4b08c351650b921bf3846d8038c7f08735
jped <jonathanped@gmail.com>
2022-12-29 19:26:47 -0500

    setup

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import find_packages, setup
 
 setup(
     name="promptlayer",
-    description="PromptLayer is a package to keep track of your LM models training",
+    description="PromptLayer is a package to keep track of your GPT models training",
     author_email="hello@magniv.io",
     url="https://www.magniv.io",
     project_urls={"Documentation": "https://docs.magniv.io",},
-    version="0.1.0",
+    version="0.1.1",
     py_modules=["promptlayer"],
     packages=find_packages(),
     install_requires=["requests", "openai"],

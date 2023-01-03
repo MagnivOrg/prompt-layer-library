f44a2d27f74d7065722773ac7de3fc46ebf0f050
jped <jonathanped@gmail.com>
2023-01-03 11:43:46 -0500

    changing timezone

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     author_email="hello@magniv.io",
     url="https://www.magniv.io",
     project_urls={"Documentation": "https://magniv.notion.site/Prompt-Layer-Docs-db0e6f50cacf4564a6d09824ba17a629",},
-    version="0.1.1",
+    version="0.1.2",
     py_modules=["promptlayer"],
     packages=find_packages(),
     install_requires=["requests", "openai"],

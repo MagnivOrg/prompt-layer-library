7f9df73ed1eeeabff9302456eceee849e7759c88
Jared Zoneraich <jszoneraich@gmail.com>
2023-01-26 17:21:56 -0500

    adding missing setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,20 @@
 from setuptools import find_packages, setup
 
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 setup(
     name="promptlayer",
     description="PromptLayer is a package to keep track of your GPT models training",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author="Magniv",
     author_email="hello@magniv.io",
     url="https://www.promptlayer.com",
     project_urls={"Documentation": "https://magniv.notion.site/Prompt-Layer-Docs-db0e6f50cacf4564a6d09824ba17a629",},
-    version="0.1.31",
+    version="0.1.32",
     py_modules=["promptlayer"],
     packages=find_packages(),
     install_requires=["requests", "openai"],

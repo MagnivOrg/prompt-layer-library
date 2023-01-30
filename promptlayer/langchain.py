552c37165750943fe6d0e38bcc55ce66c524a9ff
jped <jonathanped@gmail.com>
2023-01-30 16:24:10 -0500

    adding support for langchain generate

--- promptlayer/langchain.py
+++ promptlayer/langchain.py
@@ -1,5 +1,5 @@
 from promptlayer.promptlayer import PromptLayerBase
 
 class PromptLayer(PromptLayerBase):
-    def __init__(self, *args, provider_type="langchain", **kwargs):
-        super().__init__(*args, provider_type=provider_type, **kwargs)
+    def __init__(self, *args, provider_type="langchain", function_name="langchain", **kwargs):
+        super().__init__(*args, provider_type=provider_type, function_name=function_name, **kwargs)
--- promptlayer/promptlayer.py
+++ promptlayer/promptlayer.py
@@ -10,8 +10,12 @@ class PromptLayerBase(object):
         object.__setattr__(self, "_provider_type", provider_type)
         object.__setattr__(self, "pl_tags", pl_tags)
         if provider_type == "langchain":
-            object.__setattr__(self, "_identifying_params", obj._identifying_params)
-            object.__setattr__(self, "_function_name", f"langchain.{obj.__repr_name__()}")
+            if hasattr(obj, "_identifying_params"):
+                object.__setattr__(self, "_identifying_params", obj._identifying_params)
+            else:
+                object.__setattr__(self, "_identifying_params", {})
+            if hasattr(obj, "__repr_name__"):
+                object.__setattr__(self, "_function_name", f"{object.__getattribute__(self, '_function_name')}.{obj.__repr_name__()}")
 
     def __getattr__(self, name):
         return PromptLayerBase(
@@ -39,22 +43,43 @@ class PromptLayerBase(object):
         request_start_time = datetime.datetime.now().timestamp()
         response = object.__getattribute__(self, "_obj")(*args, **kwargs)
         request_end_time = datetime.datetime.now().timestamp()
-        if object.__getattribute__(self, "_provider_type") == "langchain":
+        is_langchain = object.__getattribute__(self, "_provider_type") == "langchain"
+        if is_langchain:
             kwargs = {**kwargs, **object.__getattribute__(self, "_identifying_params")}
-        request_response = requests.post(
-            "https://api.promptlayer.com/track-request",
-            json={
-                "function_name": object.__getattribute__(self, "_function_name"),
-                "provider_type": object.__getattribute__(self, "_provider_type"),
-                "args": args,
-                "kwargs": kwargs,
-                "tags": tags,
-                "request_response": response,
-                "request_start_time": request_start_time,
-                "request_end_time": request_end_time,
-                "api_key": get_api_key(),
-            },
-        )
-        if request_response.status_code != 200:
-            raise Exception(f"Error while tracking request: {request_response.json().get('message')}")
+        if is_langchain and response.__class__.__name__ == "LLMResult":
+            args = args[0]
+            for i in range(len(args)):
+                arg = [args[i]]
+                resp = response.generations[i]
+                request_response = requests.post(
+                    "https://api.promptlayer.com/track-request",
+                    json={
+                        "function_name": object.__getattribute__(self, "_function_name"),
+                        "provider_type": object.__getattribute__(self, "_provider_type"),
+                        "args": arg,
+                        "kwargs": kwargs,
+                        "tags": tags,
+                        "request_response": resp[0].text,
+                        "request_start_time": request_start_time,
+                        "request_end_time": request_end_time,
+                        "api_key": get_api_key(),
+                    },
+                )
+        else:
+            request_response = requests.post(
+                "https://api.promptlayer.com/track-request",
+                json={
+                    "function_name": object.__getattribute__(self, "_function_name"),
+                    "provider_type": object.__getattribute__(self, "_provider_type"),
+                    "args": args,
+                    "kwargs": kwargs,
+                    "tags": tags,
+                    "request_response": response,
+                    "request_start_time": request_start_time,
+                    "request_end_time": request_end_time,
+                    "api_key": get_api_key(),
+                },
+            )
+            if request_response.status_code != 200:
+                raise Exception(f"Error while tracking request: {request_response.json().get('message')}")
         return response

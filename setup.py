4869d74439b33ed1349eeb2444f4371858410e7d
jped <jonathanped@gmail.com>
2023-02-01 15:25:22 -0500

    version issue

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     author_email="hello@magniv.io",
     url="https://www.promptlayer.com",
     project_urls={"Documentation": "https://magniv.notion.site/Prompt-Layer-Docs-db0e6f50cacf4564a6d09824ba17a629",},
-    version="0.1.6",
+    version="0.1.61",
     py_modules=["promptlayer", "langchain"],
     packages=find_packages(),
     install_requires=["requests", "openai"],

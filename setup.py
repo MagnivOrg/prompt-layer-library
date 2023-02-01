e598cb074625e8559eab008dda7ca0893d90a4d6
jped <jonathanped@gmail.com>
2023-02-01 14:31:25 -0500

    adding langchain

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     url="https://www.promptlayer.com",
     project_urls={"Documentation": "https://magniv.notion.site/Prompt-Layer-Docs-db0e6f50cacf4564a6d09824ba17a629",},
     version="0.1.6",
-    py_modules=["promptlayer"],
+    py_modules=["promptlayer", "langchain"],
     packages=find_packages(),
     install_requires=["requests", "openai"],
 )

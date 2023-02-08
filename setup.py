6748d0814688fa09c6c2eb1ebcffe9a7d1fa0493
jped <jonathanped@gmail.com>
2023-02-08 14:46:01 -0500

    fix for windows computers

--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
 from setuptools import find_packages, setup
 
-# read the contents of your README file
-from pathlib import Path
-this_directory = Path(__file__).parent
-long_description = (this_directory / "README.md").read_text()
+try:
+    # read the contents of your README file
+    from pathlib import Path
+    this_directory = Path(__file__).parent
+    long_description = (this_directory / "README.md").read_text()
+except:
+    long_description = ""
 
 setup(
     name="promptlayer",
@@ -14,7 +17,7 @@ setup(
     author_email="hello@magniv.io",
     url="https://www.promptlayer.com",
     project_urls={"Documentation": "https://magniv.notion.site/Prompt-Layer-Docs-db0e6f50cacf4564a6d09824ba17a629",},
-    version="0.1.62",
+    version="0.1.63",
     py_modules=["promptlayer"],
     packages=find_packages(),
     install_requires=["requests", "openai", "langchain"],

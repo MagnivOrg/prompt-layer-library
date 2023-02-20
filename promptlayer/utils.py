f780bdd7fdbca88ae4c9052b8bf7e531289e2321
jped <jonathanped@gmail.com>
2023-02-20 17:22:58 -0500

    black and length finish reason

--- promptlayer/utils.py
+++ promptlayer/utils.py
@@ -13,21 +13,58 @@ def get_api_key():
     else:
         return promptlayer.api_key
 
-def promptlayer_api_handler(function_name, provider_type, args, kwargs, tags, response, request_start_time, request_end_time, api_key):
-    if isinstance(response, types.GeneratorType) or isinstance(response, types.AsyncGeneratorType):
-        return OpenAIGeneratorProxy(response, {
-                        "function_name": function_name,
-                        "provider_type": provider_type,
-                        "args": args,
-                        "kwargs": kwargs,
-                        "tags": tags,
-                        "request_start_time": request_start_time,
-                        "request_end_time": request_end_time,
-        })
+
+def promptlayer_api_handler(
+    function_name,
+    provider_type,
+    args,
+    kwargs,
+    tags,
+    response,
+    request_start_time,
+    request_end_time,
+    api_key,
+):
+    if isinstance(response, types.GeneratorType) or isinstance(
+        response, types.AsyncGeneratorType
+    ):
+        return OpenAIGeneratorProxy(
+            response,
+            {
+                "function_name": function_name,
+                "provider_type": provider_type,
+                "args": args,
+                "kwargs": kwargs,
+                "tags": tags,
+                "request_start_time": request_start_time,
+                "request_end_time": request_end_time,
+            },
+        )
     else:
-        return promptlayer_api_request(function_name, provider_type, args, kwargs, tags, response, request_start_time, request_end_time, api_key)
-    
-def promptlayer_api_request(function_name, provider_type, args, kwargs, tags, response, request_start_time, request_end_time, api_key):
+        return promptlayer_api_request(
+            function_name,
+            provider_type,
+            args,
+            kwargs,
+            tags,
+            response,
+            request_start_time,
+            request_end_time,
+            api_key,
+        )
+
+
+def promptlayer_api_request(
+    function_name,
+    provider_type,
+    args,
+    kwargs,
+    tags,
+    response,
+    request_start_time,
+    request_end_time,
+    api_key,
+):
     try:
         request_response = requests.post(
             "https://api.promptlayer.com/track-request",
@@ -45,43 +82,60 @@ def promptlayer_api_request(function_name, provider_type, args, kwargs, tags, re
         )
         if request_response.status_code != 200:
             if hasattr(request_response, "json"):
-                print(f"WARNING: While logging your request PromptLayer had the following error: {request_response.json().get('message')}", file=sys.stderr)
+                print(
+                    f"WARNING: While logging your request PromptLayer had the following error: {request_response.json().get('message')}",
+                    file=sys.stderr,
+                )
             else:
-                print(f"WARNING: While logging your request PromptLayer had the following error: {request_response}", file=sys.stderr)
-    except  Exception as e:
-        print(f"WARNING: While logging your request PromptLayer had the following error: {e}", file=sys.stderr)
+                print(
+                    f"WARNING: While logging your request PromptLayer had the following error: {request_response}",
+                    file=sys.stderr,
+                )
+    except Exception as e:
+        print(
+            f"WARNING: While logging your request PromptLayer had the following error: {e}",
+            file=sys.stderr,
+        )
+
 
 def promptlayer_get_prompt(prompt_name, api_key):
     request_response = requests.post(
-            "https://api.promptlayer.com/library-get-prompt-template",
-            json={
-                "prompt_name": prompt_name,
-                "api_key": api_key,
-            },
-        )
+        "https://api.promptlayer.com/library-get-prompt-template",
+        json={"prompt_name": prompt_name, "api_key": api_key,},
+    )
     if request_response.status_code != 200:
         if hasattr(request_response, "json"):
-            raise Exception(f"PromptLayer had the following error while getting your prompt: {request_response.json().get('message')}")
+            raise Exception(
+                f"PromptLayer had the following error while getting your prompt: {request_response.json().get('message')}"
+            )
         else:
-            raise Exception(f"PromptLayer had the following error while getting your prompt: {request_response}")
+            raise Exception(
+                f"PromptLayer had the following error while getting your prompt: {request_response}"
+            )
     return request_response.json()
+
 
 def promptlayer_publish_prompt(prompt_name, prompt_template, tags, api_key):
     request_response = requests.post(
-            "https://api.promptlayer.com/library-publish-prompt-template",
-            json={
-                "prompt_name": prompt_name,
-                "prompt_template": prompt_template,
-                "tags": tags,
-                "api_key": api_key,
-            },
-        )
+        "https://api.promptlayer.com/library-publish-prompt-template",
+        json={
+            "prompt_name": prompt_name,
+            "prompt_template": prompt_template,
+            "tags": tags,
+            "api_key": api_key,
+        },
+    )
     if request_response.status_code != 200:
         if hasattr(request_response, "json"):
-            raise Exception(f"PromptLayer had the following error while publishing your prompt: {request_response.json().get('message')}")
+            raise Exception(
+                f"PromptLayer had the following error while publishing your prompt: {request_response.json().get('message')}"
+            )
         else:
-            raise Exception(f"PromptLayer had the following error while publishing your prompt: {request_response}")
+            raise Exception(
+                f"PromptLayer had the following error while publishing your prompt: {request_response}"
+            )
     return request_response.json()
+
 
 class OpenAIGeneratorProxy:
     def __init__(self, generator, api_request_arguments):
@@ -91,7 +145,7 @@ class OpenAIGeneratorProxy:
 
     def __iter__(self):
         return self
-    
+
     def __aiter__(self):
         return self
 
@@ -99,14 +153,17 @@ class OpenAIGeneratorProxy:
         result = await self.generator.__anext__()
         self.results.append(result)
         return self._abstracted_next(result)
-    
+
     def __next__(self):
         result = next(self.generator)
         return self._abstracted_next(result)
 
     def _abstracted_next(self, result):
         self.results.append(result)
-        if result.choices[0].finish_reason == "stop":
+        if (
+            result.choices[0].finish_reason == "stop"
+            or result.choices[0].finish_reason == "length"
+        ):
             promptlayer_api_request(
                 self.api_request_arugments["function_name"],
                 self.api_request_arugments["provider_type"],
@@ -119,11 +176,11 @@ class OpenAIGeneratorProxy:
                 get_api_key(),
             )
         return result
-    
+
     def cleaned_result(self):
         response = ""
         for result in self.results:
-            response = f'{response}{result.choices[0].text}'
+            response = f"{response}{result.choices[0].text}"
         final_result = self.results[-1]
         final_result.choices[0].text = response
         return final_result

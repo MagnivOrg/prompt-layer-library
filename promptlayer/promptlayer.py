fada23ad635fd6acf058bc77a13fdc2401412075
jped <jonathanped@gmail.com>
2023-03-14 10:22:17 -0400

    testing pl_tags

--- promptlayer/promptlayer.py
+++ promptlayer/promptlayer.py
@@ -25,6 +25,8 @@ class PromptLayerBase(object):
     def __call__(self, *args, **kwargs):
         from promptlayer.utils import get_api_key, promptlayer_api_handler
         tags = kwargs.pop("pl_tags", None)
+        if tags is not None and not isinstance(tags, list):
+            raise Exception("pl_tags must be a list of strings.")
         return_pl_id = kwargs.pop("return_pl_id", False) 
         request_start_time = datetime.datetime.now().timestamp()
         function_object = object.__getattribute__(self, "_obj")

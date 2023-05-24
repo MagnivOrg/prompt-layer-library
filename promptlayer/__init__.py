8e0b45ac05d14b29ad20b45e56df30128a10592c
AbuBakar <abubakarsohail123@gmail.com>
2023-05-24 20:45:51 +0500

    anthropic support

--- promptlayer/__init__.py
+++ promptlayer/__init__.py
@@ -1,15 +1,21 @@
-from promptlayer.promptlayer import PromptLayerBase
+import os
+
+import anthropic
+import openai
+
 import promptlayer.langchain as langchain
 import promptlayer.prompts as prompts
 import promptlayer.track as track
-import openai
-import os
+from promptlayer.promptlayer import PromptLayerBase
 
 api_key = os.environ.get("PROMPTLAYER_API_KEY")
 openai = PromptLayerBase(openai, function_name="openai")
 
+anthropic_client = PromptLayerBase(
+    anthropic.Client(""),
+    function_name="anthropic",
+    provider_type="anthropic",
+)
 
-__all__ = [
-    "api_key",
-    "openai",
-]
+
+__all__ = ["api_key", "openai", "anthropic_client"]

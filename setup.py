2975af53bd6703a90c4cfefb0b67d692f9d53406
jped <jonathanped@gmail.com>
2023-07-25 15:20:32 -0400

    updating version

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     project_urls={
         "Documentation": "https://magniv.notion.site/Prompt-Layer-Docs-db0e6f50cacf4564a6d09824ba17a629",
     },
-    version="0.1.92",
+    version="0.1.93",
     py_modules=["promptlayer"],
     packages=find_packages(),
     install_requires=["requests", "langchain"],

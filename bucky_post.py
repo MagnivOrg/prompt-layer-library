42ef5524f2917c7d383cf365d9c8c02efe1e00d8
buckyroberts <buckyroberts@gmail.com>
2024-08-05 14:41:56 -0400

    Example script

--- bucky_post.py
+++ bucky_post.py
@@ -1,12 +1,26 @@
 from promptlayer import PromptLayer
 
-promptlayer_client = PromptLayer(api_key="pl_7b93c14d8a188035bcaa52e61d9b8c58")
-OpenAI = promptlayer_client.openai.OpenAI
-client = OpenAI()
+pl = PromptLayer(api_key="pl_7b93c14d8a188035bcaa52e61d9b8c58")
 
 
+@pl.traceable()
+def beans(qty):
+    import time
+
+    time.sleep(1)
+    return f"{qty} beans"
+
+
+@pl.traceable()
+def apples(qty):
+    beans_response = beans(qty)
+    return f"{beans_response} apples"
+
+
+@pl.traceable()
 def get_response(input_variables):
-    response = promptlayer_client.run(
+    apples(5)
+    response = pl.run(
         prompt_name="ai-poet",
         prompt_release_label="prod",
         input_variables=input_variables,

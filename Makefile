b65fbd72c8881a18a2d031306d7bfad01e519019
buckyroberts <buckyroberts@gmail.com>
2024-08-15 12:57:13 -0400

    Bump version and add test

--- Makefile
+++ Makefile
@@ -1,3 +1,7 @@
 .PHONY: lint
 lint:
 	poetry run pre-commit run --all-files
+
+.PHONY: test
+test:
+	poetry run pytest
--- promptlayer/__init__.py
+++ promptlayer/__init__.py
@@ -1,4 +1,4 @@
 from .promptlayer import PromptLayer
 
-__version__ = "1.0.12"
+__version__ = "1.0.13"
 __all__ = ["PromptLayer", "__version__"]

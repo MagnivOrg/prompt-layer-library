fbe69041fcce54871501c89afd1ffc2cb4c9f65b
NoamBenS <me@noambensimon.com>
2026-05-11 19:14:02 -0400

    1.4.3 -> 1.4.4

--- promptlayer/__init__.py
+++ promptlayer/__init__.py
@@ -16,7 +16,7 @@ from .exceptions import (
 )
 from .promptlayer import AsyncPromptLayer, PromptLayer
 
-__version__ = "1.4.3"
+__version__ = "1.4.4"
 __all__ = [
     "PromptLayer",
     "AsyncPromptLayer",
